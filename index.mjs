63e4e9cd98844fec378102aa624af20bc87863e1
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2024-12-13 13:10:36 -0300

    Added a few new comments

--- index.mjs
+++ index.mjs
@@ -42,28 +42,25 @@ export default class Tree {
 
   // Function to remove a item
   deleteItem(value, currentNode = this.root) {
-    if (currentNode === null) return currentNode; // Caso base: árbol vacío
+    if (currentNode === null) return currentNode;
 
-    // Recorrer el árbol
     if (value < currentNode.value) {
-      // El valor está en el subárbol izquierdo
       currentNode.left = this.deleteItem(value, currentNode.left);
     } else if (value > currentNode.value) {
-      // El valor está en el subárbol derecho
       currentNode.right = this.deleteItem(value, currentNode.right);
     } else {
-      // Nodo encontrado, manejar los casos de eliminación
+      // Node found, handle the different cases of remove
       if (currentNode.left === null) {
-        // Caso 1: Nodo tiene un solo hijo o ninguno (derecho o ninguno)
+        // Case 1: Node have 1 child or none (left or none)
         return currentNode.right;
       } else if (currentNode.right === null) {
-        // Caso 2: Nodo tiene un hijo izquierdo
+        // Caso 2: Node have a right child
         return currentNode.left;
       } else {
-        // Caso 3: Nodo tiene dos hijos
-        const successor = this.findMin(currentNode.right); // Buscar el menor en el subárbol derecho
-        currentNode.value = successor.value; // Reemplazar datos
-        currentNode.right = this.deleteItem(successor.value, currentNode.right); // Eliminar sucesor
+        // Caso 3: Node have 2 childs
+        const successor = this.findMin(currentNode.right); // Find the minor in the right subtree
+        currentNode.value = successor.value; // Replace value
+        currentNode.right = this.deleteItem(successor.value, currentNode.right); // Remove successor
       }
     }
 
@@ -94,9 +91,7 @@ export default class Tree {
     return null;
   }
 
-  // Function that accepts a callback function as it's parameter.
-  // levelOrder should traverse the tree in breadth-first level order
-  // and call the callback on each node as it traverses.
+  // Function that traverse the tree in breadth-first level order
   levelOrder(callback) {
     if (!callback) return "You need a callback function";
 
@@ -111,7 +106,6 @@ export default class Tree {
     }
   }
 
-  // Primero el subarbol izquierdo, luego el nodo actual y por ultimo el subarbol derecho
   inOrder(callback, currentNode = this.root) {
     if (!callback) return "You need a callback function";
 
@@ -122,7 +116,6 @@ export default class Tree {
     this.inOrder(callback, currentNode.right);
   }
 
-  // Primero el nodo actual, luego el subarbol izquierdo y por ultimo el subarbol derecho
   preOrder(callback, currentNode = this.root) {
     if (!callback) return "You need a callback function";
 
@@ -134,7 +127,6 @@ export default class Tree {
     this.preOrder(callback, currentNode.right);
   }
 
-  // Primero el subarbol izquierdo, luego el subarbol derecho y por ultimo el nodo actual
   postOrder(callback, currentNode = this.root) {
     if (!callback) return "You need a callback function";
 
@@ -147,23 +139,20 @@ export default class Tree {
 
   // Function that returns the given node's height
   height(node) {
-    // Primero, encuentra el nodo inicial utilizando `find`
+    // Find the node
     const startNode = this.find(node);
-    if (!startNode) return -1; // Si no se encuentra, retorna -1
+    if (!startNode) return -1; // If not found, return -1
 
-    // Define una función recursiva interna para calcular la altura
     const calculateHeight = (currentNode) => {
-      if (!currentNode) return -1; // Caso base: nodo nulo tiene altura -1
+      if (!currentNode) return -1;
 
-      // Calcula recursivamente las alturas de los subárboles izquierdo y derecho
       const leftHeight = calculateHeight(currentNode.left);
       const rightHeight = calculateHeight(currentNode.right);
 
-      // La altura del nodo actual es 1 + el máximo entre las alturas de los subárboles
       return 1 + Math.max(leftHeight, rightHeight);
     };
 
-    // Llama a la función recursiva a partir del nodo inicial
+    // Call the function recursively from the node
     return calculateHeight(startNode);
   }
 
@@ -204,12 +193,12 @@ export default class Tree {
   // Function that rebalances an unbalanced tree
   rebalance() {
     const sortedValues = [];
-    this.inOrder((node) => sortedValues.push(node.value));
+    this.inOrder((node) => sortedValues.push(node.value)); // Add all the nodes to an array
     this.root = buildTree(sortedValues);
   }
 }
 
-// Transforma todos los elementos en nodos y crea el arbol
+// Transform all the elements into nodes, and create the tree
 function buildTree(array) {
   if (array.length === 0) return null;
 
