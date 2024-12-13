337e93150ad3e1271967f747fecea207356e4522
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2024-12-13 13:11:38 -0300

    Implemented some tests

--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import Tree from "./index.mjs";
 
+// Function that returns the tree in the console
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -13,36 +14,50 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-// function debugTree(node) {
-//     if (node === null) return;
-//     console.log(`Node: ${node.value}, Left: ${node.left?.value}, Right: ${node.right?.value}`);
-//     debugTree(node.left);
-//     debugTree(node.right);
-// }
+// Returns an array with random numbers
+function randomCleanAndSortArray(minSize, maxSize, maxValue) {
+  const arraySize = Math.floor(Math.random() * (maxSize - minSize) + minSize);
+  const randomNumbers = [];
 
-function cleanAndSortArray(array) {
-  // Eliminar duplicados y ordenar el array
-  return [...new Set(array)].sort((a, b) => a - b);
+  while (randomNumbers.length < arraySize) {
+    randomNumbers.push(Math.floor(Math.random() * maxValue));
+  }
+
+  return [...new Set(randomNumbers)].sort((a, b) => a - b);
 }
 
-let sortedArray = cleanAndSortArray([
-  1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
-]);
+// Tests
+const sortedArray = randomCleanAndSortArray(5, 11, 100);
+console.log(sortedArray);
 
 const myTree = new Tree(sortedArray);
-
-// console.log(`Array: ${sortedArray}`);
-
-// console.log("-------");
-
 prettyPrint(myTree.root);
+console.log(`It's balanced? ${myTree.isBalanced()}`);
 
-// myTree.insert(2);
-// console.log("----------");
+console.log("Level order:");
+myTree.levelOrder((node) => console.log(node.value));
 
-// prettyPrint(myTree.root);
+console.log("Pre order:");
+myTree.preOrder((node) => console.log(node.value));
 
-myTree.deleteItem(67);
-console.log("----------");
+console.log("Post order:");
+myTree.postOrder((node) => console.log(node.value));
 
+console.log("In order:");
+myTree.inOrder((node) => console.log(node.value));
+
+myTree.insert(104);
+myTree.insert(150);
+myTree.insert(127);
+myTree.insert(145);
+myTree.insert(190);
 prettyPrint(myTree.root);
+console.log(`It's balanced? ${myTree.isBalanced()}`);
+
+myTree.rebalance();
+prettyPrint(myTree.root);
+console.log(`It's balanced? ${myTree.isBalanced()}`);
+
+const sarray = [10, 5, 15, 3, 7, 13, 17];
+const tree = new Tree(sarray);
+console.log(tree.inOrder((node) => console.log(node.value)));
